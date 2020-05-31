9bd69b299e5558d9aeadc54a3b227e4b5b070239
hogucc <uku.h1r8@gmail.com>
2020-05-31 19:10:13 +0900

    Userモデルにvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :user_name, presence: true, length: { maximum: 30 }
+  validates :self_introduction, length: { maximum: 200 }
+  validates :postal_code, length: { is: 7 }
 end
