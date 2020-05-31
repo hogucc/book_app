0b81727dc80ba511f94bda7710d056c1a7e8aa3d
hogucc <uku.h1r8@gmail.com>
2020-05-31 22:37:12 +0900

    after_sign_in_path_forメソッドに不足していた引数を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :postal_code, :address, :self_introduction])
     end
 
-    def after_sign_in_path_for
+    def after_sign_in_path_for(resource_or_scope)
       books_path
     end
 end
