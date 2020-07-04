8321431faeb084245a62a1798a5725de4f7d5f13
hogucc <uku.h1r8@gmail.com>
2020-07-04 10:34:01 +0900

    reportsをscaffoldで作成

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_133826) do
+ActiveRecord::Schema.define(version: 2020_07_01_105758) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +59,13 @@ ActiveRecord::Schema.define(version: 2020_06_15_133826) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
