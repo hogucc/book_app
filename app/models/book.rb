0641a5b4f36482e05b6478dd590f865a9df937fb
hogucc <uku.h1r8@gmail.com>
2020-07-10 23:26:25 +0900

    booksに対するコメント機能を実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+  include Commentable
   mount_uploader :picture, PictureUploader
   paginates_per 5
   has_and_belongs_to_many :users
+  has_many :comments, as: :commentable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   resources :reports do
     resources :comments, only: [:create, :edit, :update, :destroy], module: :reports
   end
-  devise_for :users, controllers: {
-              registrations: "users/registrations",
-              omniauth_callbacks: "users/omniauth_callbacks"
-  }
-  resources :books
+
+  resources :books do
+    resources :comments, only: [:create, :edit, :update, :destroy], module: :books
+  end
+  
   resources :users, only: [:show, :index, :edit, :update]
   resources :users do
     resources :followings, only: [:index], controller: "users/followings"
